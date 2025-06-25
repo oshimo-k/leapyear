23c3d1cfbd5b1b9eb9f3fa6db2ff76276d4d7a58
team02 <team02@example.com>
2025-06-25 03:02:21 +0000

    Implement leapyear function for subtask

--- leapyear_test.go
+++ leapyear_test.go
@@ -5,8 +5,40 @@ import (
 )
 
 func TestLeapYear01(t *testing.T) {
-    expected := leapyaer(1)
+    expected := leapyear(1)
+    actual := false
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+
+func TestLeapYear02(t *testing.T) {
+    expected := leapyear(2004)
     actual := true
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+
+func TestLeapYear03(t *testing.T) {
+    expected := leapyear(2000)
+    actual := true
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+
+func TestLeapYear04(t *testing.T) {
+    expected := leapyear(2100)
+    actual := false
+    if expected != actual {
+        t.Errorf("Error")
+    }
+}
+
+func TestLeapYear05(t *testing.T) {
+    expected := leapyear(2005)
+    actual := false
     if expected != actual {
         t.Errorf("Error")
     }
